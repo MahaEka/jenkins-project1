0bf9df8e24760a2a9ed847b3cf197928c5d16572
Mi Era <miera@Mis-MacBook-Air.attlocal.net>
2025-10-03 22:57:32 -0500

    Update pom.xml and App.java; add infra directory

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins pipeline!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 LOVE YOU !";
     }
 }
